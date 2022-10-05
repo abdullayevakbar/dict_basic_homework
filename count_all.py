b7b3c7cd864753391c50d87d348677da3681e3a0
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-10-05 15:26:42 +0500

    dictionary

--- count_all.py
+++ count_all.py
@@ -1,6 +1,3 @@
-from curses.ascii import isalpha, isdigit
-
-
 def count_all(txt):
     """
     Given a function that takes a string and calculates the number of letters and digits within it.
@@ -12,8 +9,11 @@ def count_all(txt):
     """
     a = b = 0
     for i in txt:
-        if isalpha(i):
+        if i.isalpha():
             a += 1
-        if isdigit(i):
+        if i.isdigit():
             b += 1
-    return {"letters": a, "digits": b}
+    return {"LETTERS": a, "DIGITS": b}
+
+
+print(count_all("1nhnk134"))
