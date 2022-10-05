941b94c96f3d64e6ac27a38aad6da964d4f579e9
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-10-05 22:04:38 +0500

    dictionary

--- oldest_person.py
+++ oldest_person.py
@@ -1,4 +1,4 @@
-def oldest(people:dict):
+def oldest(people: dict):
     """
     Given a dictionary containing the names and ages of a group of people, return the name of the oldest person.
     Args:
@@ -6,4 +6,11 @@ def oldest(people:dict):
     Returns:
         str: the name of the oldest person
     """
-    return
+    a = 0
+    ans = ""
+    for i, j in people:
+        if (a < i):
+            ans = j
+            a = i
+
+    return ans
